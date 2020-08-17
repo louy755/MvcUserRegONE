538be25e1713f5fa1a7613da581ca4a15f4d88d4
louy755 <lumberjack1695@yahoo.com>
2020-08-17 06:18:13 -0600

    Add project files.

---
+++ Models/UserClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcUserRegONE.Models
+{
+    public class UserClass
+    {
+        
+        [Required(ErrorMessage = "Enter Username !")]
+        [Display(Name = "Enter Username :")]
+        [StringLength(maximumLength: 7, MinimumLength = 3, ErrorMessage = "Username Length Must be Max 7 & Min 3")]
+        public string Uname { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your Email Address")]
+        [Display(Name = "Email Id")]
+        public string Uemail { get; set; }
+
+        [Required(ErrorMessage ="Enter your Password!")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Upwd { get; set; }
+
+        [Required(ErrorMessage = "Enter your Password!")]
+        [Display(Name = "Re-Password")]
+        [DataType(DataType.Password)]
+        [Compare("Upwd")]
+        public string Repwd { get; set; }
+
+        [Required(ErrorMessage ="Select Gender Please!")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage ="Upload your Image Please")]
+        [Display(Name = " Profile Image")]
+        public string Uimg { get; set; }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcUserRegONE.Startup))]
+namespace MvcUserRegONE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
